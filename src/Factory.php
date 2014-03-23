964a97da094df9d436505d95c79b2c5155655b37
Cees-Jan <ceesjank@gmail.com>
2014-03-23 20:09:13 +0100

    More docblocks

--- src/Factory.php
+++ src/Factory.php
@@ -1,7 +1,20 @@
 <?php
 
+/*
+ * This file is part of HtmlCompress.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace WyriHaximus\HtmlCompress;
 
+/**
+ * Class Factory
+ *
+ * @package WyriHaximus\HtmlCompress
+ */
 class Factory {
 
     public static function construct($html) {
--- src/Parser.php
+++ src/Parser.php
@@ -1,7 +1,20 @@
 <?php
 
+/*
+ * This file is part of HtmlCompress.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace WyriHaximus\HtmlCompress;
 
+/**
+ * Class Parser
+ *
+ * @package WyriHaximus\HtmlCompress
+ */
 class Parser {
 
     protected $html = '';
