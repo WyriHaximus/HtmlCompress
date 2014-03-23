796ba53a6dd61f3dc82ba30e7cd8b744b4c9e6a8
Cees-Jan <ceesjank@gmail.com>
2014-03-23 20:03:36 +0100

    Docblocks

--- src/Compressor/CompressorInterface.php
+++ src/Compressor/CompressorInterface.php
@@ -1,7 +1,25 @@
 <?php
 
+/*
+ * This file is part of HtmlCompress.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace \WyriHaximus\HtmlCompress\Compressor;
 
+/**
+ * Interface CompressorInterface
+ * @package WyriHaximus\HtmlCompress\Compressor
+ */
 interface CompressorInterface {
+
+    /**
+     * @param $string
+     *
+     * @return string
+     */
     public function compress($string);
 }
--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -1,7 +1,20 @@
 <?php
 
+/*
+ * This file is part of HtmlCompress.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace WyriHaximus\HtmlCompress\Tests;
 
+/**
+ * Class FactoryTest
+ *
+ * @package WyriHaximus\HtmlCompress\Tests
+ */
 class FactoryTest extends \PHPUnit_Framework_TestCase {
 
     public function testConstruct() {
