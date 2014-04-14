59b35cf2cf8b6d7a0f34b4f68e0e5d6cf368a9de
Cees-Jan <ceesjank@gmail.com>
2014-04-14 21:07:34 +0200

    Patterns list

---
+++ src/Patterns.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of HtmlCompress.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace WyriHaximus\HtmlCompress;
+
+/**
+ * Class Patterns
+ *
+ * @package WyriHaximus\HtmlCompress
+ */
+class Patterns {
+
+    const MATCH_PRE       = '!(<pre[^>]*>?)(.*?)(</pre>)!is';
+    const MATCH_TEXTAREA  = '!(<textarea[^>]*>?)(.*?)(</textarea>)!is';
+    const MATCH_JSCRIPT   = '!(<(script|script(.*?)type=("|\')text/javascript("|\')[^>]*)>)(.*?)(</script>)!is';
+    const MATCH_SCRIPT    = '!(<script[^>]*>?)(.*?)(</script>)!is';
+
+}
