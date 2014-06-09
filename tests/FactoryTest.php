6212aa2443d95b6e401108699074c964c0ecc4fd
Cees-Jan <ceesjank@gmail.com>
2014-06-09 14:49:02 +0200

    Only test that the factory returns a parser

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -20,7 +20,6 @@ class FactoryTest extends \PHPUnit_Framework_TestCase {
     public function testConstruct() {
         $parser = \WyriHaximus\HtmlCompress\Factory::construct('<html><p> <span>foo bar</span> </p></html>html>');
         $this->assertInstanceOf('WyriHaximus\HtmlCompress\Parser', $parser);
-        $this->assertSame('<html><p><span>foo bar</span></p></html>html>', $parser->compress());
     }
 
 }
