2463d1a07a529310fbe04bfbcea3735f54dd8725
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-06-17 22:48:17 +0200

    Note sure why but I wrote all the arrays in that file as pre 5.4 arrays, guess old habbits die hard

--- tests/PatternsTest.php
+++ tests/PatternsTest.php
@@ -20,206 +20,206 @@ use WyriHaximus\HtmlCompress\Patterns;
 class PatternsTest extends \PHPUnit_Framework_TestCase {
 
     public function testPreProvider() {
-        return array(
-            array(
+        return [
+            [
                 Patterns::MATCH_PRE,
                 '<pre>awkef8227h9r8r23</pre>',
-                array(
+                [
                     '',
                     '',
-                ),
-                array(
-                    array(
+                ],
+                [
+                    [
                         '<pre>awkef8227h9r8r23</pre>',
-                    ),
-                    array(
+                    ],
+                    [
                         '<pre>',
-                    ),
-                    array(
+                    ],
+                    [
                         'awkef8227h9r8r23',
-                    ),
-                    array(
+                    ],
+                    [
                         '</pre>',
-                    ),
-                ),
-            ),
-            array(
+                    ],
+                ],
+            ],
+            [
                 Patterns::MATCH_PRE,
                 'o <pre>awkef8227h9r8r23</pre> 0',
-                array(
+                [
                     'o ',
                     ' 0',
-                ),
-                array(
-                    array(
+                ],
+                [
+                    [
                         '<pre>awkef8227h9r8r23</pre>',
-                    ),
-                    array(
+                    ],
+                    [
                         '<pre>',
-                    ),
-                    array(
+                    ],
+                    [
                         'awkef8227h9r8r23',
-                    ),
-                    array(
+                    ],
+                    [
                         '</pre>',
-                    ),
-                ),
-            ),
-            array(
+                    ],
+                ],
+            ],
+            [
                 Patterns::MATCH_PRE,
                 'o <pre attribute="bar">awkef8227h9r8r23</pre> 0',
-                array(
+                [
                     'o ',
                     ' 0',
-                ),
-                array(
-                    array(
+                ],
+                [
+                    [
                         '<pre attribute="bar">awkef8227h9r8r23</pre>',
-                    ),
-                    array(
+                    ],
+                    [
                         '<pre attribute="bar">',
-                    ),
-                    array(
+                    ],
+                    [
                         'awkef8227h9r8r23',
-                    ),
-                    array(
+                    ],
+                    [
                         '</pre>',
-                    ),
-                ),
-            ),
-            array(
+                    ],
+                ],
+            ],
+            [
                 Patterns::MATCH_TEXTAREA,
                 '<textarea>awkef8227h9r8r23</textarea>',
-                array(
+                [
                     '',
                     '',
-                ),
-                array(
-                    array(
+                ],
+                [
+                    [
                         '<textarea>awkef8227h9r8r23</textarea>',
-                    ),
-                    array(
+                    ],
+                    [
                         '<textarea>',
-                    ),
-                    array(
+                    ],
+                    [
                         'awkef8227h9r8r23',
-                    ),
-                    array(
+                    ],
+                    [
                         '</textarea>',
-                    ),
-                ),
-            ),
-            array(
+                    ],
+                ],
+            ],
+            [
                 Patterns::MATCH_TEXTAREA,
                 'o <textarea>awkef8227h9r8r23</textarea> 0',
-                array(
+                [
                     'o ',
                     ' 0',
-                ),
-                array(
-                    array(
+                ],
+                [
+                    [
                         '<textarea>awkef8227h9r8r23</textarea>',
-                    ),
-                    array(
+                    ],
+                    [
                         '<textarea>',
-                    ),
-                    array(
+                    ],
+                    [
                         'awkef8227h9r8r23',
-                    ),
-                    array(
+                    ],
+                    [
                         '</textarea>',
-                    ),
-                ),
-            ),
-            array(
+                    ],
+                ],
+            ],
+            [
                 Patterns::MATCH_TEXTAREA,
                 'o <textarea attribute="bar">awkef8227h9r8r23</textarea> 0',
-                array(
+                [
                     'o ',
                     ' 0',
-                ),
-                array(
-                    array(
+                ],
+                [
+                    [
                         '<textarea attribute="bar">awkef8227h9r8r23</textarea>',
-                    ),
-                    array(
+                    ],
+                    [
                         '<textarea attribute="bar">',
-                    ),
-                    array(
+                    ],
+                    [
                         'awkef8227h9r8r23',
-                    ),
-                    array(
+                    ],
+                    [
                         '</textarea>',
-                    ),
-                ),
-            ),
-            array(
+                    ],
+                ],
+            ],
+            [
                 Patterns::MATCH_STYLE,
                 '<style>awkef8227h9r8r23</style>',
-                array(
+                [
                     '',
                     '',
-                ),
-                array(
-                    array(
+                ],
+                [
+                    [
                         '<style>awkef8227h9r8r23</style>',
-                    ),
-                    array(
+                    ],
+                    [
                         '<style>',
-                    ),
-                    array(
+                    ],
+                    [
                         'awkef8227h9r8r23',
-                    ),
-                    array(
+                    ],
+                    [
                         '</style>',
-                    ),
-                ),
-            ),
-            array(
+                    ],
+                ],
+            ],
+            [
                 Patterns::MATCH_STYLE,
                 'o <style>awkef8227h9r8r23</style> 0',
-                array(
+                [
                     'o ',
                     ' 0',
-                ),
-                array(
-                    array(
+                ],
+                [
+                    [
                         '<style>awkef8227h9r8r23</style>',
-                    ),
-                    array(
+                    ],
+                    [
                         '<style>',
-                    ),
-                    array(
+                    ],
+                    [
                         'awkef8227h9r8r23',
-                    ),
-                    array(
+                    ],
+                    [
                         '</style>',
-                    ),
-                ),
-            ),
-            array(
+                    ],
+                ],
+            ],
+            [
                 Patterns::MATCH_STYLE,
                 'o <style attribute="bar">awkef8227h9r8r23</style> 0',
-                array(
+                [
                     'o ',
                     ' 0',
-                ),
-                array(
-                    array(
+                ],
+                [
+                    [
                         '<style attribute="bar">awkef8227h9r8r23</style>',
-                    ),
-                    array(
+                    ],
+                    [
                         '<style attribute="bar">',
-                    ),
-                    array(
+                    ],
+                    [
                         'awkef8227h9r8r23',
-                    ),
-                    array(
+                    ],
+                    [
                         '</style>',
-                    ),
-                ),
-            ),
-        );
+                    ],
+                ],
+            ],
+//        ];
     }
 
     /**
