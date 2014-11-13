d23526d71eca8501d26b003eff6d7c15c84ce93c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-11-13 07:57:51 +0100

    Added a script tag to the basic and fastest examples

--- examples/basic.php
+++ examples/basic.php
@@ -3,4 +3,4 @@
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $parser = \WyriHaximus\HtmlCompress\Factory::construct();
-echo $parser->compress("<html>\r\n\t<body>\r\n\t\t<h1>Title</h1>\r\n\t</body>\r\n</html>\r\n"), PHP_EOL;
+echo $parser->compress("<html>\r\n\t<body>\r\n\t\t<h1>Title</h1>\r\n\t<script>\r\n\t\talert(' iqeugfo wef we');\r\n\t</script>\r\n\t</body>\r\n</html>\r\n"), PHP_EOL;
