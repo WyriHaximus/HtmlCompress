ddedaa794725c104398d7a81e17ef075617ed5c0
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-12-26 15:51:58 +0100

    ld+json test

---
+++ tests/SpecialFormats/LdJsonTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of HtmlCompress.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace WyriHaximus\HtmlCompress\Tests\SpecialFormats;
+
+use WyriHaximus\HtmlCompress\Compressor;
+
+class LdJsonTest extends \PHPUnit_Framework_TestCase
+{
+    public function javascriptCompressorProvider()
+    {
+        return [
+            [
+                new Compressor\JSMinCompressor(),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider javascriptCompressorProvider
+     */
+    public function testLdJson(Compressor\CompressorInterface $compressor)
+    {
+        $input = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'input' . DIRECTORY_SEPARATOR . 'ld.json.input');
+        $inputJson = $this->getJson($input);
+        $compressedInput = $compressor->compress($input);
+        $compressedJson = $this->getJson($compressedInput);
+
+        self::assertSame($inputJson, $compressedJson);
+    }
+
+    private function getJson($string)
+    {
+        $start = strpos($string, '{');
+        $end = strrpos($string, '}') + 1;
+        $string = substr($string, $start, $end - $start);
+
+        return json_decode($string, true);
+    }
+}
