a453f6ee8b9da81e9ec8cdb5b65657b1d2428492
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-12-27 17:04:20 +0100

    JShrink compressor

---
+++ tests/Compressor/JShrinkCompressorTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wyrihaximus
+ * Date: 6/25/14
+ * Time: 5:32 PM
+ */
+
+namespace WyriHaximus\HtmlCompress\Tests\Compressor;
+
+
+use WyriHaximus\HtmlCompress\Compressor\JShrinkCompressor;
+
+class JShrinkCompressorTest extends AbstractVendorCompressorTest
+{
+    const COMPRESSOR = JShrinkCompressor::class;
+}
