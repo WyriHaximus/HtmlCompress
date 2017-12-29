4857d1bd46dd9ad574dcb0ece214121390f209b9
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-12-30 00:22:41 +0100

    YUIJSCompressor

---
+++ src/Compressor/YUIJSCompressor.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of HtmlCompress.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace WyriHaximus\HtmlCompress\Compressor;
+
+use YUI\Compressor as YUICompressor;
+
+/**
+ * Class YUIJSCompressor
+ *
+ * @package WyriHaximus\HtmlCompress\Compressor
+ */
+class YUIJSCompressor extends Compressor
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute($string)
+    {
+        $yui = new YUICompressor();
+        $yui->setType(YUICompressor::TYPE_JS);
+        return $yui->compress($string);
+    }
+}
