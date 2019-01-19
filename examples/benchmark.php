8e0c680f32ca554a2215bd6a82c3b656b9f5864c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-01-19 17:51:44 +0100

    Time the benchmark internally

--- examples/benchmark.php
+++ examples/benchmark.php
@@ -6,6 +6,10 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 $rawHtml = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'benchmark.html');
 
+echo date('r'), PHP_EOL;
+
 for ($i = 0; $i < 13; $i++) {
     Factory::constructSmallest(false)->compress($rawHtml);
 }
+
+echo date('r'), PHP_EOL;
