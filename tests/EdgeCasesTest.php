8330e9752c53fad5e0091f030ad7b689794302dc
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-06-22 12:39:53 +0200

    Add edge cases test name to data provider

--- tests/EdgeCasesTest.php
+++ tests/EdgeCasesTest.php
@@ -12,17 +12,13 @@ final class EdgeCasesTest extends TestCase
 {
     public function providerEdgeCase(): array
     {
-        $baseDir = __DIR__ . \DIRECTORY_SEPARATOR . 'EdgeCases' . \DIRECTORY_SEPARATOR;
         $dirs = [];
 
-        $items = \glob($baseDir . '*', \GLOB_ONLYDIR);
-
-        if ($items === false) {
-            return $dirs;
-        }
-
-        foreach ($items as $item) {
-            $dirs[] = [$item . \DIRECTORY_SEPARATOR];
+        $items = \glob(__DIR__ . \DIRECTORY_SEPARATOR . 'EdgeCases' . \DIRECTORY_SEPARATOR . '*', \GLOB_ONLYDIR);
+        if ($items !== false) {
+            foreach ($items as $item) {
+                $dirs[$item] = [$item . \DIRECTORY_SEPARATOR];
+            }
         }
 
         return $dirs;
