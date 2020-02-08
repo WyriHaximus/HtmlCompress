d98dbf878853e216f1961dc44d112852be3ad97f
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-02-08 18:18:19 +0100

    Aggressively remove spaces between tags
    Signed-off-by: Cees-Jan Kiewiet <ceesjank@gmail.com>

--- src/Compressor/HtmlCompressor.php
+++ src/Compressor/HtmlCompressor.php
@@ -14,6 +14,7 @@ final class HtmlCompressor implements CompressorInterface
     public function __construct(Patterns $patterns)
     {
         $this->htmlMin = new HtmlMin();
+        $this->htmlMin->doRemoveSpacesBetweenTags(true);
         $this->htmlMin->attachObserverToTheDomLoop(new HtmlMinObserver($patterns));
     }
 
